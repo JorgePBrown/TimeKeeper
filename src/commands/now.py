70323baf841806730dbd10d58d91051688444fa7
Jorge Brown <jorge.pc.brown@gmail.com>
2021-12-02 17:44:21 +0000

    now command now shows current duration

--- src/commands/now.py
+++ src/commands/now.py
@@ -1,11 +1,13 @@
 from commands.command import Command
 from tasks import Task
+from utils import sum_time
+from time import time as now
 
 class NowCommand(Command):
     def exec(self, tasks: Task, args) -> Task:
         ongoing = tasks.ongoing_task
         if ongoing is not None:
-            print(f"Current task is: {ongoing['name']}")
+            print(f"Current task: {ongoing['name']} - {sum_time(now()-ongoing['start'])}")
         else:
             print("No task being performed right now.")
         return tasks
